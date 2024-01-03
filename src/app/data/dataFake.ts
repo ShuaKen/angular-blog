01e1914a45dafedf7493b9404ac0e228fdc50e89
ShuaKen <joshu.887@gmail.com>
2024-01-03 16:39:01 -0300

    content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "title": "CONFRONTO EM FAMILIA",
+    "description": "No dia 19 de pai(Darth varder) e filho (luke Skywalker) tem um confronto de sabre de luz",
+    "photoCover": "https://media.contentapi.ea.com/content/dam/eacom/en-us/migrated-images/2016/11/news-article-images-star-wars-goh-splash.jpg.adapt.crop16x9.1023w.jpg"
+  },
+  {
+    "id":"2",
+    "title": "Resgate da Princesa Leia",
+    "description": "Testeeee teste ",
+    "photoCover": "https://s2-techtudo.glbimg.com/JC6OYuKMag1PHbTedsk-Ay_aRlI=/0x0:1200x722/888x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_08fbf48bc0524877943fe86e43087e7a/internal_photos/bs/2023/r/T/A9hOoeSzqSmJAz2jReyA/capa-star-wars.jpg"
+  }
+]
